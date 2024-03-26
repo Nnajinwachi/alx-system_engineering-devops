29663e74ad994ed3577b522f370ab48e096b9871
Nnajinwachi <nwachukwunnaji40@gmail.com>
2024-03-26 12:06:38 -0700

    all

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+#puppet for ssh config
+
+file_line{'Turn off password authentication':
+path => '/etc/ssh/ssh_config',
+line => 'PasswordAuthentication no'
+}
+
+file_line{'The identity file':
+path => '/etc/ssh/ssh_config',
+line => 'IdentityFile ~/.ssh/school'
+}
